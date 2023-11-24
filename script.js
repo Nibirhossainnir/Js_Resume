76a342ac18c7957790f7780cf92d72468c9d702c
Nibirhossainnir <144318970+Nibirhossainnir@users.noreply.github.com>
2023-11-25 00:40:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const fileInput = document.querySelector("input[type=file]");
+const output = document.querySelector(".output");
+
 var n = navigator.getBattery().then((n)=>{
     
-    document.write("Charge "+n.level*100+"%");
+    document.write(`Charging:${n.charging}<br>ChargingTime:${n.chargingTime}<br>DischargingTime:${n.dischargingTime}<br>Level:${n.level*100}%<br>Onchargingchange:${n.onchargingchange}<br>Onchargingtimechange:${n.onchargingtimechange}<br>Ondischargingtimechange:${n.ondischargingtimechange}<br>Onlevelchange:${n.onlevelchange}`);
+    console.log(n);
 });
-
 
 
 
